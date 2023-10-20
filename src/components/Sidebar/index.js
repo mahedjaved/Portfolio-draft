85b1928f91cd3fdb63600efdbe0ca5c5a21e2590
mahedjaved <mahed95@gmail.com>
2023-10-20 08:55:16 +0100

    [sidebar] adds the home logo below main logo in the sidebar]

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,9 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => (
     <div className='nav-bar'>
@@ -10,6 +12,14 @@ const Sidebar = () => (
             <img className='sub-logo' src={LogoSubtitle} alt="slobodan" />
 
         </Link>
+        
+        {/* adding a NavLink below the Logo present in the NavBar, this will link to every single page, for that exact needs to be true, for rest keep false*/}
+        <nav>
+            {/* the first NavLink will be the Home page */}
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
+            </NavLink>
+        </nav>
     </div>
 )
 
