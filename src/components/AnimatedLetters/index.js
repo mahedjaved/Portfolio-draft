88f3fde7e861caee0ed0412f9a1a8200dc2333db
mahedjaved <mahed95@gmail.com>
2023-10-23 10:11:41 +0100

    [home] adds the animated letters useState hook]

--- src/components/AnimatedLetters/index.js
+++ src/components/AnimatedLetters/index.js
@@ -1,20 +1,78 @@
-/**
- * @desc: purpose of this class is to have letters in the main page pop out seperately. This function takes an array of letters as inputs and outputs animated letters
- * @output: [Array type] an array of animated letters
- */
-
+import { Link } from 'react-router-dom'
+import LogoTitle from '../../assets/images/logo-s.png'
 import './index.scss'
-const AnimatedLetters = ({ letterClass, strArray, idx }) => {
+import { useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+
+const Home = () => {
+  // pass in the AnimatedLetters as a useState hook
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  // initialise characters to aniamte
+  const nameArray = ['m', 'a', 'h', 'e', 'd']
+  const jobArray = [
+    'a',
+    's',
+    'p',
+    'i',
+    'r',
+    'i',
+    'n',
+    'g',
+    ' ',
+    'a',
+    'i',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
   return (
-    // span since it is inline element
-    <span>
-      {strArray.map((char, i) => (
-        <span key={char + i} className={`${letterClass} _${i + idx}`}>
-          {char}
-        </span>
-      ))}
-    </span>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          {/* main name in h1 title */}
+          <h1>
+            {/* just a cool way to setting animation delay property */}
+            <span className={`${letterClass} _12`}>I</span>
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+
+            {/* the logo before the rest of the name follows */}
+            <img src={LogoTitle} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+          </h1>
+
+          {/* a h2 title for what I do*/}
+          <h2>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h2>
+
+          {/* Link to contacts page */}
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default AnimatedLetters
+export default Home
